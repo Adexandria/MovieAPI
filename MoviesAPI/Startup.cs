290accde4e428e7defb3f9749b6a54acd8d1ee1f
Adeola Aderibigbe <adeolaaderibigbe09@gmail.com>
2021-01-19 11:59:39 +0100

    Added dependency injection for both interfaces

--- MoviesAPI/Startup.cs
+++ MoviesAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MoviesAPI.Services;
 
 namespace MoviesAPI
 {
@@ -26,6 +27,10 @@ namespace MoviesAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSingleton<IMovies, MoviesRepository>();
+            
+            
+            services.AddSingleton<IUser, UserRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
