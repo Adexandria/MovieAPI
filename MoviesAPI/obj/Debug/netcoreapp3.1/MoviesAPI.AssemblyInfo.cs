86feb1c5d1953fdc3ff23472014b14cadc1af6c0
Adexandria <adeolaaderibigbe09@gmail.com>
2021-05-21 18:24:50 +0100

    MovieAPI

--- MoviesAPI/obj/Debug/netcoreapp3.1/MoviesAPI.AssemblyInfo.cs
+++ MoviesAPI/obj/Debug/netcoreapp3.1/MoviesAPI.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("144ce9b1-ed84-476d-9b96-2275445cafab")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoviesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
