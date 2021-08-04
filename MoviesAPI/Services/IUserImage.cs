9bc3368fddf4dd517a50448ced64b7401d4d95c0
Adexandria <adeolaaderibigbe09@gmail.com>
2021-08-04 13:46:38 +0100

    created interface and respository for userimage

---
+++ MoviesAPI/Services/IUserImage.cs
@@ -0,0 +1,13 @@
+﻿using MoviesAPI.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesAPI.Services
+{
+    public interface IUserImage 
+    {
+        Task<UserImage> AddUserImage(string userId,string url);
+    }
+}
---
+++ MoviesAPI/Services/UserImageRespository.cs
@@ -0,0 +1,27 @@
+﻿using MoviesAPI.UserModel;
+using System;
+using System.Threading.Tasks;
+
+namespace MoviesAPI.Services
+{
+    public class UserImageRespository : IUserImage
+    {
+        readonly MovieDb db;
+        public UserImageRespository(MovieDb db)
+        {
+            this.db = db;
+        }
+        public async Task<UserImage> AddUserImage(string userId, string url)
+        {
+            UserImage userImage = new UserImage
+            {
+                ImageId = Guid.NewGuid(),
+                UserImageURL = url,
+                UserId = userId
+            };
+            await db.UserImage.AddAsync(userImage);
+            await db.SaveChangesAsync();
+            return userImage;
+        }
+    }
+}
