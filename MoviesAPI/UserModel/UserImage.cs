db3308620ee49cb0b6fa823badc89b5d545a854e
Adexandria <adeolaaderibigbe09@gmail.com>
2021-08-04 13:21:12 +0100

    created UserImage class

---
+++ MoviesAPI/UserModel/UserImage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesAPI.UserModel
+{
+    public class UserImage
+    {
+        [Key]
+        public Guid ImageId { get; set; }
+        public string UserImageURL { get; set; }
+        [ForeignKey("AspNetUsers")]
+        public int UserId { get; set; }
+    }
+}
