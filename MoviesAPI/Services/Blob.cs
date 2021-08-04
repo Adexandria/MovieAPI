d3de84f10cc594b5f03b9880ea9fbec8d9b5c116
Adexandria <adeolaaderibigbe09@gmail.com>
2021-08-04 13:42:00 +0100

    BLob client Interface and repository

---
+++ MoviesAPI/Services/Blob.cs
@@ -0,0 +1,44 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesAPI.Services
+{
+    public class Blob : IBlob
+    {
+        private readonly string Container = "textimages";
+        private readonly BlobServiceClient _blobServiceClient;
+        public Blob(BlobServiceClient blobServiceClient)
+        {
+            _blobServiceClient = blobServiceClient;
+        }
+
+        public async Task Upload(IFormFile model)
+        {
+            var blobClient = GetBlobServiceClient(model.FileName);
+            await blobClient.UploadAsync(model.OpenReadStream(), overwrite: true);
+
+        }
+        public Uri GetUri(string filename) 
+        {
+            var blobClient = GetBlobServiceClient(filename);
+            if (blobClient.ExistsAsync().Result)
+            {
+                var s = blobClient.Uri;
+                return s;
+            }
+            
+            return null;
+        }
+        private BlobClient GetBlobServiceClient(string name) 
+        {
+            var blobContainer = _blobServiceClient.GetBlobContainerClient(Container);
+            var blobClient = blobContainer.GetBlobClient(name);
+            return blobClient;
+        }
+    }
+}
+
---
+++ MoviesAPI/Services/IBlob.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MoviesAPI.Services
+{
+    public  interface IBlob
+    {
+        Task Upload(IFormFile model);
+        Uri GetUri(string filename);
+    }
+}
